c5d965c448cdd36ad2c387489bf506d9492488ee
unknown <isigalit@gmail.com>
2021-04-07 21:41:48 +0300

    moving BrowserRouter

--- src/App.js
+++ src/App.js
@@ -8,37 +8,41 @@ import Connect from './components/Connect/Connect';
 import Clalit from './components/Clalit/Clalit';
 import 'fontsource-roboto';
 
-import { Route } from 'react-router-dom';
+import { BrowserRouter,Route } from 'react-router-dom';
 
 
 function App() {
   return (
     <div className="h-100 d-flex flex-column jutify-content-between">
       <Header />
-     
-        <Route path="/" exact>
-          <div className="row justify-content-center ">
-            <Home />
-          </div>
-        </Route>
 
-        <Route path="/about" exact>
-          <div className="row justify-content-center ">
-            <About />
-          </div>
-        </Route>
+      <BrowserRouter basename="/good_eye_optic">
 
-        <Route path="/connect" exact>
-          <div className="row justify-content-center ">
-            <Connect />
-          </div>
-        </Route>
+          <Route path="/" exact>
+            <div className="row justify-content-center ">
+              <Home />
+            </div>
+          </Route>
 
-        <Route path="/clalit" exact>
-          <div className="row justify-content-center ">
-            <Clalit />
-          </div>
-        </Route>
+          <Route path="/about" exact>
+            <div className="row justify-content-center ">
+              <About />
+            </div>
+          </Route>
+
+          <Route path="/connect" exact>
+            <div className="row justify-content-center ">
+              <Connect />
+            </div>
+          </Route>
+
+          <Route path="/clalit" exact>
+            <div className="row justify-content-center ">
+              <Clalit />
+            </div>
+          </Route>
+        
+      </BrowserRouter>
         
       <Footer />
 
--- src/index.js
+++ src/index.js
@@ -3,16 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
 
 
 ReactDOM.render(
 
-  <BrowserRouter basename="/good_eye_optic">
     <React.StrictMode>
       <App />
-    </React.StrictMode>
-    </BrowserRouter>,
+    </React.StrictMode>,
   document.getElementById('root')
 );
 
